b2145bd5368f9bbf0ffcf04cf7396c95c370fbf6
Amit <amitchaudhary.adc@gmail.com>
2024-06-28 09:17:51 +0530

    Upgrading KtLint Version for spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,38 @@ plugins {
 }
 
 apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
+val ktLintVersion = "1.3.0"
+
+spotless {
+    kotlin {
+        target("**/*.kt")
+        targetExclude("**/build/**/*.kt")
+        ktlint(ktLintVersion).editorConfigOverride(
+            mapOf(
+                "ktlint_code_style" to "android",
+                "ij_kotlin_allow_trailing_comma" to true,
+                "disabled_rules" to
+                        "filename," +
+                        "annotation,annotation-spacing," +
+                        "argument-list-wrapping," +
+                        "double-colon-spacing," +
+                        "enum-entry-name-case," +
+                        "multiline-if-else," +
+                        "no-empty-first-line-in-method-block," +
+                        "package-name," +
+                        "trailing-comma," +
+                        "spacing-around-angle-brackets," +
+                        "spacing-between-declarations-with-annotations," +
+                        "spacing-between-declarations-with-comments," +
+                        "unary-op-spacing"
+            )
+        )
+        licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+    }
+    format("kts") {
+        target("**/*.kts")
+        targetExclude("**/build/**/*.kts")
+        // Look for the first line that doesn't have a block comment (assumed to be the license)
+        licenseHeaderFile(rootProject.file("spotless/copyright.kt"), "(^(?![\\/ ]\\*).*$)")
+    }
+}
